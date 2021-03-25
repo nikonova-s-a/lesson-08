3a399bd7cd265220ccc100c586afeb338bbff72c
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2021-03-25 14:31:31 +0300

    Add colors variables

--- front/config/webpack.config.js
+++ front/config/webpack.config.js
@@ -132,6 +132,10 @@ module.exports = function (webpackEnv) {
                 },
                 stage: 3
               }),
+              require('postcss-simple-vars')({ 
+                silent: true,
+                variables: require('./colors')
+              }),
               // Adds PostCSS Normalize as the reset css with default options,
               // so that it honors browserslist config in package.json
               // which in turn let's users customize the target behavior as per their needs.
