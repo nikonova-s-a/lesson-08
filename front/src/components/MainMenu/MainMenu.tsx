5ddd36aeb7073abe2aad4c141d4d56c9cd47a22a
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2021-04-14 01:17:30 +0300

    Fixing Main Menu

--- front/src/components/MainMenu/MainMenu.tsx
+++ front/src/components/MainMenu/MainMenu.tsx
@@ -1,6 +1,8 @@
 import block from 'bem-cn'
-import React from 'react'
+import { useState } from 'react'
 import './MainMenu.css'
+import { MainMenuItem } from './MainMenuItem/MainMenuItem'
+import { MainMenuItemName } from './MainMenuItem/MainMenuItemName'
 
 interface Props {
 }
@@ -8,10 +10,44 @@ interface Props {
 const b = block('main-menu')
 
 export const MainMenu: React.FC<Props> = () => {
+  const [activeMenuItem, setActiveMenuItem] = useState(MainMenuItemName.Catalog);
+
   return (
     <nav className={b()}>
-      <a href="/catalog" className={b('item')}>Каталог</a>
-      <a href="/ref" className={b('item', { active: true })}>Справочники</a>
+      <MainMenuItem
+        name={MainMenuItemName.Catalog}
+        to='/catalog'
+        active={MainMenuItemName.Catalog === activeMenuItem}
+        onClick={() => setActiveMenuItem(MainMenuItemName.Catalog)}
+      />
+
+      <MainMenuItem
+        name={MainMenuItemName.Authors}
+        to='/ref/authors'
+        active={MainMenuItemName.Authors === activeMenuItem}
+        onClick={() => setActiveMenuItem(MainMenuItemName.Authors)}
+      />
+
+      <MainMenuItem
+        name={MainMenuItemName.Genres}
+        to='/ref/genres'
+        active={MainMenuItemName.Genres === activeMenuItem}
+        onClick={() => setActiveMenuItem(MainMenuItemName.Genres)}
+      />
+
+      <MainMenuItem
+        name={MainMenuItemName.Languages}
+        to='/ref/languages'
+        active={MainMenuItemName.Languages === activeMenuItem}
+        onClick={() => setActiveMenuItem(MainMenuItemName.Languages)}
+      />
+
+      <MainMenuItem
+        name={MainMenuItemName.Publishers}
+        to='/ref/publishers'
+        active={MainMenuItemName.Publishers === activeMenuItem}
+        onClick={() => setActiveMenuItem(MainMenuItemName.Publishers)}
+      />
     </nav>
   )
 }
